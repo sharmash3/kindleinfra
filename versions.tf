9fb9231eef8f2b03e29a9f624712a76088aedcf7
Suraj SHARMA <ssharmaparfr@gmail.com>
2025-04-13 19:43:40 +0200

    enabled GA

--- versions.tf
+++ versions.tf
@@ -1,18 +1,22 @@
 terraform {
+  required_version = ">= 1.5.7"  # To ensure you're using Terraform version 1.5.7 or higher
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      
+      version = "4.26.0"  # Specify the version you're using for the azurerm provider
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
+      version = "2.36.0"  # Specify the version you're using for the kubernetes provider
     }
     helm = {
       source = "hashicorp/helm"
+      version = "2.17.0"  # Specify the version you're using for the helm provider
     }
     random = {
       source = "hashicorp/random"
+      version = "3.7.1"  # Specify the version you're using for the random provider
     }
   }
-
 }
